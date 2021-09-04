682c3bb0963d00a21bbb330877b60a44a1abf7a0
jasbel <asbel.elmec@gmail.com>
2021-09-03 23:28:34 -0400

    Finish Maximilian Crud example typescript

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -1,13 +1,13 @@
 import {Router} from 'express'
-import { createTodo, getTodos } from '../controller/todo.controller';
+import { createTodo, deleteTodo, getTodos, updateTodo } from '../controller/todo.controller';
 const router = Router();
 
 router.post('/', createTodo);
 
 router.get('/', getTodos);
 
-router.patch('/:id');
+router.patch('/:id', updateTodo);
 
-router.delete('/:id');
+router.delete('/:id', deleteTodo);
 
 export default router;
